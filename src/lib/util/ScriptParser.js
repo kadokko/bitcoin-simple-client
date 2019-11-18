48e581491107244514a3e6c19ba31b3235c13d3f
kadokko <smsmdev@gmail.com>
2019-11-18 23:07:52 +0900

    add comment

--- src/lib/util/ScriptParser.js
+++ src/lib/util/ScriptParser.js
@@ -10,22 +10,22 @@ class ScriptParser {
       const byteDec = parseInt(byteHex, 16);
       let dataLength = 0;
       switch (true) {
-        case byteDec <= 75:
+        case byteDec <= 75: // 0x4b
           dataLength = byteDec * 2;
           asm += scriptHex.substr(i + 2, dataLength);
           i += (2 + dataLength);
           break;
-        case byteDec === 76:
+        case byteDec === 76: // 0x4c
           dataLength = parseInt(scriptHex.substr(i + 2, 2), 16) * 2;
           asm += scriptHex.substr(i + 2 + 2, dataLength);
           i += (2 + 2 + dataLength);
           break;
-        case byteDec === 77:
+        case byteDec === 77: // 0x4d
           dataLength = parseInt(scriptHex.substr(i + 2, 4), 16) * 2;
           asm += scriptHex.substr(i + 2 + 4, dataLength);
           i += (2 + 4 + dataLength);
           break;
-        case byteDec === 78:
+        case byteDec === 78: // 0x4e
           dataLength = parseInt(scriptHex.substr(i + 2, 8), 16) * 2;
           asm += scriptHex.substr(i + 2 + 8, dataLength);
           i += (2 + 8 + dataLength);
